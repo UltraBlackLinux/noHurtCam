dbda3576339b796f7ed6da46e6e8a4191ff72116
UltraBlackLinux <accoundshit@protonmail.com>
2021-04-12 20:42:57 +0200

    - Fixed License - Added Readme

---
+++ README.md
@@ -0,0 +1,7 @@
+[![GPLv3 license](https://img.shields.io/badge/License-GPLv3-blue.svg)](http://perso.crans.org/besson/LICENSE.html)
+
+[![forthebadge](https://forthebadge.com/images/badges/built-with-love.svg)](https://forthebadge.com)
+
+# NoHurtCam
+
+Allows you to toggle the hurtcam via the press of a keybind
--- src/main/java/me/ultrablacklinux/nohurtcam/NoHurtCam.java
+++ src/main/java/me/ultrablacklinux/nohurtcam/NoHurtCam.java
@@ -5,7 +5,6 @@ import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.options.KeyBinding;
-import net.minecraft.client.options.StickyKeyBinding;
 import net.minecraft.text.Text;
 import org.lwjgl.glfw.GLFW;
 
